8c8efb24a2edfa3930817aa92a67a4e648aeb3fe
Adaeze Ndupu <andupu4@gmail.com>
2022-10-15 16:29:43 +0100

    Footer Component Implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,90 @@
+import Logo from '../assets/footerlogo.png';
+import '../stylesheets/shared.css';
+
 export const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer>
+      <section className='footer-wrapper'>
+        <div className='item1'>
+          <img src={Logo} alt='Footer Logo' />
+        </div>
+        <div className='item2'>
+          <ul>
+            <p className='footer-head'>Quick links</p>
+            <li>
+              <a href='./question.html'>Questions</a>
+            </li>
+            <li>
+              <a href='./FAQ.html'>FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <div className='item3'>
+          <ul>
+            <p className='footer-head'>Company</p>
+            <li>
+              <a href='./ourteam.html'>About</a>
+            </li>
+            <li>
+              <a href='http://'>Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <div className='item4'>
+          <ul>
+            <p className='footer-head'>Legal</p>
+            <li>
+              <a href='http://'>Privacy Policy</a>
+            </li>
+            <li>
+              <a href='http://'>Terms</a>
+            </li>
+          </ul>
+        </div>
+        <div className='item5'>
+          <ul>
+            <p className='footer-head'>Socials</p>
+            <li>
+              <a href='http://' target='_blank' rel='noopener noreferrer'>
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a href='http://' target='_blank' rel='noopener noreferrer'>
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a href='http://' target='_blank' rel='noopener noreferrer'>
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <div className='hr' />
+      {/* NEWSLETTER SECTION */}
+      <div className='newsletter'>
+        <div className='newsletter-brief'>
+          <h4>Subscribe to our newsletter</h4>
+          <p>
+            The latest news, articles, and resources, sent to your inbox weekly.
+          </p>
+        </div>
+        <div className='subscribe-email'>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Enter your email...'
+          />
+          <label htmlFor='email'>Subscribe</label>
+        </div>
+      </div>
+      <div className='hr' />
+      <p className='right-reserved'>
+        © Copyright 2022 CodeAsk. All Rights Reserved.
+      </p>
+    </footer>
+  );
 };
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,25 @@
+import React from 'react';
+import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 
 export const LandingPage = () => {
-  return <Header />;
+  return (
+    <React.Fragment>
+      <Header />
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <h1>HELLO PEOPLE OF THE WORLD</h1>
+      <Footer />
+    </React.Fragment>
+  );
 };
