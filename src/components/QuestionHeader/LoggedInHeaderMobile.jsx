f278922cf8bb9f5b5d6289d175d6196b56db48fa
Adaeze Ndupu <andupu4@gmail.com>
2022-11-03 00:23:04 +0100

    Mobile search icon functional

--- src/components/QuestionHeader/LoggedInHeaderMobile.jsx
+++ src/components/QuestionHeader/LoggedInHeaderMobile.jsx
@@ -5,26 +5,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 import UserAvatar from '../../assets/Ajiboye.png';
+import { useState } from 'react';
 
 const LoggedInHeaderMobile = () => {
+  const [showMobileSearch, setShowMobileSearch] = useState(false);
+
+  const handleShowMobileSearch = () => {
+    setShowMobileSearch(!showMobileSearch);
+  };
   return (
-    <section className='question-header-mobile'>
-      <div className='logo'>
-        <Link to='/'>
-          <img src={WhiteLogo} alt='' />
-        </Link>
-      </div>
-      <div className='mobile-notification'>
-        <div className='mobile-icons'>
-          <FontAwesomeIcon
-            icon={faSearch}
-            className='search-icon-mobile white'
-          />
-          <FontAwesomeIcon icon={faBell} className='white' />
+    <>
+      <section className='question-header-mobile'>
+        <div className='logo'>
+          <Link to='/'>
+            <img src={WhiteLogo} alt='' />
+          </Link>
         </div>
-        <img src={UserAvatar} alt='' className='userAvatar' />
-      </div>
-    </section>
+        <div className='mobile-notification'>
+          <div className='mobile-icons'>
+            <FontAwesomeIcon
+              onClick={handleShowMobileSearch}
+              icon={faSearch}
+              className='search-icon-mobile white'
+            />
+            <FontAwesomeIcon icon={faBell} className='white' />
+          </div>
+          <img src={UserAvatar} alt='' className='userAvatar' />
+        </div>
+      </section>
+      {showMobileSearch ? (
+        <div className='mobile-search-bar'>
+          <input type='text' placeholder='Search...' />
+        </div>
+      ) : null}
+    </>
   );
 };
 
