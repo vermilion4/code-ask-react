d63c50cc58fc55e5960c38cb115d146aabac39f1
Adaeze Ndupu <andupu4@gmail.com>
2022-11-02 15:14:34 +0100

    Dashboard fix

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,6 +4,9 @@ import { LoggedInMobile } from '../../components/Sidebar/LoggedInMobile';
 import NavIcon from '../../components/QuestionHeader/NavIcon';
 import { LoggedInSidebar } from '../../components/Sidebar/LoggedInSidebar';
 import '../../stylesheets/dashboard.css';
+import UserImage from '../../assets/Ajiboye-big.png';
+import { Link, NavLink } from 'react-router-dom';
+import { questionList } from '../../Data/questionList';
 
 const Dashboard = ({
   hover,
@@ -36,11 +39,11 @@ const Dashboard = ({
 
   if (windowSize.innerWidth > 900) {
     closeContent = {
-      marginLeft: '150px',
-      width: '85%',
+      marginLeft: '250px',
+      width: '80%',
     };
     openContent = {
-      marginLeft: '0px',
+      marginLeft: '85px',
     };
   } else {
     closeContent = {
@@ -61,39 +64,100 @@ const Dashboard = ({
         <LoggedInSidebar hover={hover} handleHover={handleHover} />
         <LoggedInHeader />
 
-        <div
-          className='dash-body'
-          style={hoverState ? closeContent : openContent}
-          id='dashboard-mainpage'
-        >
-          <div className='user-details'>
-            <div className='user'>
-              <img alt="Muideen's photo" className='profileImage' />
-              <div className='details'>
-                <h1 id='signed_name' />
-                <h3>Backend Developer</h3>
-                <div className='icons'>
-                  <a href='#'>
-                    <i className='fab fa-twitter' />
-                  </a>
-                  <a href='#'>
-                    <i className='fab fa-github' />
-                  </a>
-                  <a href='#'>
-                    <i className='fa fa-clock-o' />
-                  </a>
-                  <p>last seen this week</p>
-                  <a href='#'>
-                    <i className='fa fa-map-marker' />
-                  </a>
-                  <p>Abuja</p>
+        <div id='dashboard-mainpage'>
+          <div
+            className='dash-body'
+            style={hoverState ? closeContent : openContent}
+          >
+            <div className='user-details'>
+              <div className='user'>
+                <div className='profileImage'>
+                  <img alt="Muideen's photo" src={UserImage} />
+                </div>
+                <div className='details'>
+                  <h1 id='signed_name'>Ajiboye Muideen</h1>
+                  <h3>Backend Developer</h3>
+                  <div className='icons'>
+                    <div className='user-social-links'>
+                      <a href='#'>
+                        <i className='fab fa-twitter' />
+                      </a>
+                      <a href='#'>
+                        <i className='fab fa-github' />
+                      </a>
+                    </div>
+                    <div className='last-seen-location-container'>
+                      <div className='last-seen'>
+                        <a href='#'>
+                          <i className='fa fa-clock-o' />
+                        </a>
+                        <p>last seen this week</p>
+                      </div>
+                      <div className='location'>
+                        <a href='#'>
+                          <i className='fa fa-map-marker' />
+                        </a>
+                        <p>Abuja</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button className='edit-btn'>
+                <i className='fa fa-pencil' />
+                Edit Details
+              </button>
+            </div>
+            <div className='dashboard-mainpage'>
+              <div className='stats-col'>
+                <h1>Stats</h1>
+                <div className='stat-numbers'>
+                  <div className='answer'>
+                    <h3>3</h3>
+                    <p>Answers</p>
+                  </div>
+                  <div className='question'>
+                    <h3>0</h3>
+                    <p>Questions</p>
+                  </div>
+                </div>
+              </div>
+              <div className='about-col'>
+                <h1>About</h1>
+                <p>
+                  Backend Developer with special interest in building cloud
+                  infrastructure
+                </p>
+                <div className='top-post'>
+                  <div className='pre-list'>
+                    <h1>Top posts</h1>
+                    <div className='pre-list-links'>
+                      <NavLink to={'/dashboard'} className='pre-list-link'>
+                        All
+                      </NavLink>
+                      <NavLink to={'/questions'} className='pre-list-link'>
+                        Questions
+                      </NavLink>
+                      <NavLink to={'/ask-question'} className='pre-list-link'>
+                        Answers
+                      </NavLink>
+                    </div>
+                  </div>
+                  <div className='question-lists'>
+                    {questionList.map(({ question, date }) => {
+                      return (
+                        <div className='question-list'>
+                          <Link to={''}>{question}</Link>
+                          <Link to={''} href>
+                            {date}
+                          </Link>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
               </div>
             </div>
-            <button className='edit-btn'>
-              <i className='fa fa-pencil' />
-              Edit Details
-            </button>
           </div>
         </div>
         <NavIcon onclick={mobileNav} />
