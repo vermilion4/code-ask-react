2c6fb42ad7284af7e7f3ee8960a810e588ea96bd
Nweke Chidi <nwekechidigodwin460@gmail.com>
2022-11-23 09:32:32 +0100

    add draft editor
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- src/components/hooks/useAxiosPrivate.js
+++ src/components/hooks/useAxiosPrivate.js
@@ -1,14 +1,12 @@
 import { axiosPrivate } from "../baseURL";
 import { useEffect } from "react";
 import { useAuth } from "./useAuth";
-import useRefreshToken from "./useRefresh";
 
 const useAxiosPrivate = () => {
   const { auth } = useAuth();
-  const refresh = useRefreshToken()
 
   useEffect(() => {
-     axiosPrivate.interceptors.request.use(
+    const requestIntercept = axiosPrivate.interceptors.request.use(
       (config) => {
         if (!config.headers["Authorization"]) {
           config.headers["Authorization"] = `Bearer ${auth.accessToken}`;
@@ -18,28 +16,74 @@ const useAxiosPrivate = () => {
       (error) => Promise.reject(error)
     );
 
-     axiosPrivate.interceptors.response.use(
-        response => response,
-        async (error) => {
-            const prevRequest = error?.config;
-            if (error?.response?.status === 401 && !prevRequest?.sent) {
-                prevRequest.sent = true;
-                const newAccessToken = await refresh();
-                prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-                return axiosPrivate(prevRequest);
-            }
-            return Promise.reject(error);
-        }
-    );
+    // const responseIntercept = axiosPrivate.interceptors.response.use(
+    //     response => response,
+    //     async (error) => {
+    //         const prevRequest = error?.config;
+    //         if (error?.response?.status === 403 && !prevRequest?.sent) {
+    //             prevRequest.sent = true;
+    //             const newAccessToken = await refresh();
+    //             prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+    //             return axiosPrivate(prevRequest);
+    //         }
+    //         return Promise.reject(error);
+    //     }
+    // );
 
     return () => {
-      // axiosPrivate.interceptors.request.eject(requestIntercept);
+      axiosPrivate.interceptors.request.eject(requestIntercept);
       // axiosPrivate.interceptors.response.eject(responseIntercept);
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [auth]);
 
   return axiosPrivate;
 };
 
 export default useAxiosPrivate;
+
+
+// import { axiosPrivate } from "../baseURL";
+// import { useEffect } from "react";
+// import { useAuth } from "./useAuth";
+// import useRefreshToken from "./useRefresh";
+
+// const useAxiosPrivate = () => {
+//   const { auth } = useAuth();
+//   const refresh = useRefreshToken()
+
+//   useEffect(() => {
+//      axiosPrivate.interceptors.request.use(
+//       (config) => {
+//         if (!config.headers["Authorization"]) {
+//           config.headers["Authorization"] = `Bearer ${auth.accessToken}`;
+//         }
+//         return config;
+//       },
+//       (error) => Promise.reject(error)
+//     );
+
+//      axiosPrivate.interceptors.response.use(
+//         response => response,
+//         async (error) => {
+//             const prevRequest = error?.config;
+//             if (error?.response?.status === 401 && !prevRequest?.sent) {
+//                 prevRequest.sent = true;
+//                 const newAccessToken = await refresh();
+//                 prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+//                 return axiosPrivate(prevRequest);
+//             }
+//             return Promise.reject(error);
+//         }
+//     );
+
+//     return () => {
+//       // axiosPrivate.interceptors.request.eject(requestIntercept);
+//       // axiosPrivate.interceptors.response.eject(responseIntercept);
+//     };
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [auth]);
+
+//   return axiosPrivate;
+// };
+
+// export default useAxiosPrivate;
