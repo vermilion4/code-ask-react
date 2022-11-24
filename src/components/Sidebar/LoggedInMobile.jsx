6d0407d55ca85c7f84bd767daa806115a46e1d1b
Nweke Chidi <nwekechidigodwin460@gmail.com>
2022-11-24 18:24:47 +0100

    fix dynamic render of logged in
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- src/components/Sidebar/LoggedInMobile.jsx
+++ src/components/Sidebar/LoggedInMobile.jsx
@@ -6,10 +6,16 @@ import { loggedInNavData } from '../../Data/loggedInNavData';
 import Logo from '../../assets/WhiteLogo.png';
 import Logout from '../../assets/logout.png';
 import Avatar from '../../assets/Avatar.png';
+import { useAuth } from "../../components/hooks/useAuth.js";
+import SigninIcon from "../../assets/question-modal-icons/bx_log-in.png";
+import SignupIcon from "../../assets/question-modal-icons/Vector.png";
 
 export const LoggedInMobile = ({ onclick }) => {
+  const { user } = useAuth();
+  console.log(user);
   return (
     <React.Fragment>
+    
       <div id='mySidebar' className={'sidebar mobile'}>
         <div className='sidebar-logo'>
           <Link to={'/'}>
@@ -21,11 +27,13 @@ export const LoggedInMobile = ({ onclick }) => {
             onClick={onclick}
           />
         </div>
+        {user.auth?
+        <div>
         <div className='sidebar-profile'>
-          <img src={Avatar} alt='icon' />
+          <img src={user.profile_image} alt='icon' />
           <div className='sidebar-details'>
-            <h3>Muideen Ajiboye</h3>
-            <p>Edit Profile</p>
+            <h6>{user.username}</h6>
+            <p>{user.role}</p>
           </div>
         </div>
         <div className='sidebar-links'>
@@ -47,7 +55,39 @@ export const LoggedInMobile = ({ onclick }) => {
             <span className='icon-text'>Logout</span>
           </Link>
         </div>
-      </div>
+        </div>
+     :  
+            <div className="loggedout-mobile">
+              <div className="sign-in">
+                <div>
+                  <img
+                    src={SigninIcon}
+                    alt="signin icon"
+                  />
+                  <div className="modal-text-1">
+
+
+                    <Link to={"/sign-in"}>Sign In</Link>
+
+                   
+                  </div>
+                </div>
+              </div>
+              <div className="sign-up">
+                <div>
+                  <img
+                    src={SignupIcon}
+                    alt="signup icon"
+                  />
+                  <div className="modal-text-2">
+                  
+                    <Link to={"/sign-up"}>Sign Up</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
+          </div>
     </React.Fragment>
   );
 };
